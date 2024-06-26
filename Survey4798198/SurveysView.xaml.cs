5f0dcec97f009cfb5963c23ba51b85a1cf8b2499
Esme <Esme@DESKTOP-7NA17H9>
2024-06-26 15:27:54 -0600

    Se modificó el archivo SurveysView XAML, junto a su clase para agregar el evento button.

--- Survey4798198/SurveysView.xaml.cs
+++ Survey4798198/SurveysView.xaml.cs
@@ -6,4 +6,9 @@ public partial class SurveysView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AddSurveyButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SurveyDetailsView());
+    }
 }
