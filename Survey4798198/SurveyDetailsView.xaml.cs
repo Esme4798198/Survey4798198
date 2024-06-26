d2251641f22b2fc1af2e87a13fa5865c09430d48
Esme <Esme@DESKTOP-7NA17H9>
2024-06-26 14:45:57 -0600

    Se agregaron dos archivos nuevos XAML. Uno llamado SurveysView y SurveyDetailsView. Y agregar código en App.xaml.cs.

---
+++ Survey4798198/SurveyDetailsView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Survey4798198;
+
+public partial class SurveyDetailsView : ContentPage
+{
+	public SurveyDetailsView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Survey4798198/SurveysView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Survey4798198;
+
+public partial class SurveysView : ContentPage
+{
+	public SurveysView()
+	{
+		InitializeComponent();
+	}
+}
